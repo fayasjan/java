52b1ea6f0e2406e88e9b0b485fa0e5704c6aa8cb
Fayasjan B <144842372+fayasjan@users.noreply.github.com>
2024-11-14 06:05:39 +0530

    Update matrixmulti.java

--- matrixmulti.java
+++ matrixmulti.java
@@ -1,64 +1,76 @@
-import java.io.*;
-import java.util.*;
-public class matrixmulti{
-	public static void main(String args[]){
-		int m,n,i,j,k;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of rows : ");
-		m = sc.nextInt();
-		System.out.println("Enter the number of columns : ");
-		n = sc.nextInt();
-		int[][] a = new int[m][n];
-		System.out.println("Enter the Elements of 1st Matrix : ");
-		for(i=0;i<m;i++){
-			for(j=0;j<n;j++){
-				System.out.print("["+i+"]["+j+"] = ");
-				a[i][j] = sc.nextInt();
-			}
-		}
-		
-		int[][] b = new int[m][n];
-		System.out.println("Enter the Elements of 2nd Matrix : ");
-		for(i=0;i<m;i++){
-			for(j=0;j<n;j++){
-				System.out.print("["+i+"]["+j+"] = ");
-				b[i][j] = sc.nextInt();
-			}
-		}
-		
-		System.out.println("1st Martix : ");
-		for(i=0;i<m;i++){
-			for(j=0;j<n;j++){
-				System.out.print(a[i][j]+"\t");
-			}
-		System.out.println();
-		}
-		
-		System.out.println("2nd Martix : ");
-		for(i=0;i<m;i++){
-			for(j=0;j<n;j++){
-				System.out.print(b[i][j]+"\t");
-			}
-		System.out.println();
-		}
-		
-		int[][] c = new int[m][n];
-		for(i=0;i<m;i++){
-			for(j=0;j<n;j++){
-				c[i][j] = 0;
-				for(k=0;k<n;k++){
-					c[i][j] = c[i][j] + a[i][k]*b[k][j];
-				}
-			}
-		}
-		
-		System.out.println("Multiplied Martix : ");
-		for(i=0;i<m;i++){
-			for(j=0;j<n;j++){
-				System.out.print(c[i][j]+"\t");
-			}
-		System.out.println();
-		}
-		
-	}
-}	
+import java.util.Scanner;
+
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows for the first matrix: ");
+        int m1 = sc.nextInt();
+        System.out.println("Enter the number of columns for the first matrix: ");
+        int n1 = sc.nextInt();
+
+        System.out.println("Enter the number of rows for the second matrix: ");
+        int m2 = sc.nextInt();
+        System.out.println("Enter the number of columns for the second matrix: ");
+        int n2 = sc.nextInt();
+
+        if (n1 != m2) {
+            System.out.println(
+                    "Matrix multiplication is not possible. The number of columns in the first matrix must equal the number of rows in the second matrix.");
+            return;
+        }
+
+        int[][] a = new int[m1][n1];
+        System.out.println("Enter the elements of the first matrix:");
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < n1; j++) {
+                System.out.print("[" + i + "][" + j + "] = ");
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        int[][] b = new int[m2][n2];
+        System.out.println("Enter the elements of the second matrix:");
+        for (int i = 0; i < m2; i++) {
+            for (int j = 0; j < n2; j++) {
+                System.out.print("[" + i + "][" + j + "] = ");
+                b[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("First Matrix:");
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < n1; j++) {
+                System.out.print(a[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Second Matrix:");
+        for (int i = 0; i < m2; i++) {
+            for (int j = 0; j < n2; j++) {
+                System.out.print(b[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        int[][] c = new int[m1][n2];
+
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < n2; j++) {
+                c[i][j] = 0;
+                for (int k = 0; k < n1; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+
+        System.out.println("Resulting Matrix after Multiplication:");
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < n2; j++) {
+                System.out.print(c[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
